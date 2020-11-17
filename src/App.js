4540f9e423706210ddfa305345a1e39b9f57605e
pp641 <prajjwalpandey641@gmail.com>
2020-11-17 20:16:58 +0530

    initial project

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Arraycounter from "./Component/ArrayCounter/Arraycounter"
+
+function App() {
+  return (
+    <div> 
+        <Arraycounter/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Component/ArrayCounter/Arraycounter.js
@@ -0,0 +1,46 @@
+import React, { useState } from 'react'
+import "./Arraycounter.css"
+function Arraycounter() {
+    const [hook , setHook] = useState([])
+    const [empty,setEmpty] = useState(false);
+    function HandleClick1()
+    {
+        var x  =  []
+        x.push(Math.floor(Math.random() * 10) + 1);
+       x.concat(hook)
+        setHook([...hook,x]);  
+        setEmpty(hook.length === 0 )  
+    }
+    function HandleClick2()
+    {
+       var arr2 = []
+       for(var i = 1; i< hook.length; i++)
+                {
+                    arr2.push(hook[i])
+                }
+        setHook(hook => arr2)
+    }
+    return (
+        <div>
+            <div className = "heading">Queue Implementation</div>
+            <button className = " button btn btn-outline-success " onClick = {HandleClick1} > Push </button>
+            <button className = " button btn btn-outline-danger" onClick = {HandleClick2} > Pop  </button>
+            <div>
+                {
+                    (hook.length === 0) ?  <div className = "empty btn btn-outline-info">Empty</div> :   <div className = "btn btn-outline-warning" >size : {hook.length}</div>
+                }
+            </div>
+            <ol>
+            {
+           hook.map( (hook1,hook2) =>
+            hook2 === 0 ? <li className = "btn btn-outline-primary li1"> {hook1}  <span> Top </span> </li> : <li className = "btn btn-outline-success li1">{hook1} <br/> </li>
+
+           )
+           }
+        </ol>
+
+        </div>
+    )
+}
+
+export default Arraycounter;
